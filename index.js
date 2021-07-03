d7046a9fd7e9a303e1c89f41bc46bea5210f16b9
kumaran <kumaran26skr@gmail.com>
2021-07-03 08:54:27 +0200

    feat: Printing the output to the console and writing the text contents to a file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const csv = require('csv-parser');
+
+const startTime = new Date().getTime();
   
 let text = fs.readFileSync('t8.shakespeare.txt', 'utf8').toString();
 let words = fs.readFileSync('find_words.txt', 'utf8').split("\n");
@@ -15,6 +17,7 @@ fs.createReadStream('french_dictionary.csv').pipe(csv()).on('data', function(dat
 	wordsDictionary[word[0]] = word[1];
 }).on('end',function(){
     replaceWords();
+    output();
 });
 
 function countOccurences(text, word) {
@@ -27,3 +30,15 @@ function countOccurences(text, word) {
          text = text.replace(new RegExp(word, "g"), wordsDictionary[word]);
      });
  }
+
+ function output(){
+	const endTime = new Date().getTime();
+	const time = endTime - startTime;
+	const seconds = Math.floor((time/1000) % 60);
+	let memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+	
+	console.log('Time taken in seconds', seconds);
+	console.log(`The script uses approximately ${Math.round(memoryUsed * 100) / 100} MB`);
+	
+	fs.writeFileSync('t8.shakespeare.output.txt', text);
+}
